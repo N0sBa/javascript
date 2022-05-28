1873af0932d240799b1a3b9bb7df1da0f32bf689
N0sBa <n0sba.learning@gmail.com>
2022-05-28 11:28:14 +0700

    update

---
+++ udemy/practice-places-01-starting-setup/src/UI/Modal.js
@@ -0,0 +1,33 @@
+export class Modal{
+  constructor(contentId, fallbackText) {
+    this.contentTemplateEl = document.getElementById(contentId);
+    this.modalTemplateEl = document.getElementById('modal-template');
+    this.fallbackText = fallbackText;
+  }
+  
+  show() {
+    if ('content' in document.createElement('template')) {
+      const modalElements = document.importNode(this.modalTemplateEl.content, true);
+      this.modalElement = modalElements.querySelector('.modal');
+      this.backDropElement = modalElements.querySelector('.backdrop');
+
+      const contentElement = document.importNode(this.contentTemplateEl.content,true);
+      this.modalElement.appendChild(contentElement);
+      document.body.insertAdjacentElement('afterbegin', this.modalElement);
+      document.body.insertAdjacentElement('afterbegin', this.backDropElement);
+
+    } else {
+      //fallback code
+      alert(this.fallbackText);
+    }
+  }
+
+  hide() {
+    if (this.modalElement) {
+      document.body.removeChild(this.modalElement);
+      document.body.removeChild(this.backDropElement);
+      this.modalElement = null;
+      this.backDropElement = null;
+    }
+  }
+}
